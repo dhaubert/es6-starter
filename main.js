d236c56594314c51949ff99b3dc47504b5306d6d
Douglas Haubert <dhaubert.ti@gmail.com>
2018-10-18 01:23:50 -0300

    Classes

--- main.js
+++ main.js
@@ -1,9 +1,29 @@
-class TodoList {
-    constructor(){
-        this.todos = [];
+class List {
+    constructor() {
+        this.data = [];
     }
     
-    addTodo() {
-        
+    add(data) {
+        this.data.push(data);
+        console.log(this.data);
     }
+}
+
+class TodoList extends List {
+    constructor() {
+        super();
+        
+        this.user = 'Douglas';
+    }
+    
+    displayUser() {
+        console.log(this.user)
+    }
+    
+    
+}
+
+const myList = new TodoList();
+document.getElementById("new-todo").onclick = function(){
+    myList.addTodo();
 }
